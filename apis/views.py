d5fefc83fb3b3b3f2445f5b00c9fa9c6836715e1
Salman Zafar <salman.zafar@innowi.com>
2019-05-03 18:42:02 +0500

    get post put delete api implmented for articles model

--- apis/views.py
+++ apis/views.py
@@ -2,11 +2,27 @@ from rest_framework import generics
 from .models import Article
 from .serializers import ArticleSerializers
 
+
 class ArticleListView(generics.ListAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializers
 
 
 class ArticleDetailView(generics.RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializers
+
+
+class ArticleCreateView(generics.CreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializers
+
+
+class ArticleUpdateView(generics.UpdateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializers
+
+
+class ArticleDeleteView(generics.DestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializers
